6fb34b22d67ee146d86fb644de29bec8cfd6d64b
ahoang2502 <athoang252@gmail.com>
2023-12-21 07:27:08 +1100

    navbar

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function HomePage() {
+	return (
+		<div className="flex flexc-ol gap-y-4">
+			<h1>Dashboard</h1>
+		</div>
+	);
+}
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
 				primary: "bg-[#9047ff] text-white font-semibold hover:bg-[#9047ff]/80",
+				primaryBlue: "text-primary bg-blue-600 hover:bg-blue-600/80",
 			},
 			size: {
 				default: "h-10 px-4 py-2",
