d2ebc06cab3f445531e70f5688f02c956abde38a
ahoang2502 <athoang252@gmail.com>
2024-01-06 23:48:28 +1100

    search-page

---
+++ app/(browse)/search/page.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "next/navigation";
+import React, { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/Results";
+
+interface SearchPageProps {
+	searchParams: {
+		term?: string;
+	};
+}
+
+const SearchPage = ({ searchParams }: SearchPageProps) => {
+	if (!searchParams.term) redirect("/");
+
+	return (
+		<div
+			className="h-full p-8 max-w-screen-2xl ml-auto
+     "
+		>
+			<Suspense fallback={<ResultsSkeleton />}>
+				<Results term={searchParams.term} />
+			</Suspense>
+		</div>
+	);
+};
+
+export default SearchPage;
